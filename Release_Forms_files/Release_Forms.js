4fe9b7e8c12c7ca70c63a7cd27df1a4588e7c342
Kevin Fink <kevin.fink@demandmedia.com>
2012-04-06 15:46:48 -0700

    Traumhof website as of 2012-04-06

---
+++ Release_Forms_files/Release_Forms.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20090125
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return true;}
+function onPageLoad()
+{loadMozillaCSS('Release_Forms_files/Release_FormsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
